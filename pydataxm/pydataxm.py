9ba842f97281c41e092cf93a9803ecfaec04fd3e
EquipoAnaliticaXM <PJARAVA_EXT@XM.COM.CO>
2022-04-25 11:54:37 -0500

    new_version_without_using _json_file
    We released a new version of pydataxm which not require an auxiliar file to request the available metrics.
    
    This new file include  method named all_variables that return a list of variables available.

--- pydataxm/pydataxm.py
+++ pydataxm/pydataxm.py
@@ -1,27 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 12 11:05:46 2022
+
+@author: 1040750676
+"""
+
 import requests
 import json
 import pandas as pd
 import datetime as dt
+import time 
 # noinspection SpellCheckingInspection
 
 
 class ReadDB(object):
-    
+
     def __new__(cls):
         return super(ReadDB, cls).__new__(cls)
     
+    
     def __init__(self):
-        """This object was created to extract data from API XM"""
-        
+        """This object was created to extract data from API XM"""   
         self.url = "http://servapibi.xm.com.co/{period_base}"
         self.connection = None
         self.request = ''
-        self.lectura = requests.get(r'https://raw.githubusercontent.com/EquipoAnaliticaXM/API_XM/master/pydataxm/metricasAPI.json').json()
-        self.inventario_metricas = json.loads(self.lectura)
+        self.inventario_metricas = self.all_variables()
+        
+        
+    def all_variables(self):
+        """This method allows the user to get all variables availables into the API XM.
+        Args:
+            None
+        Returns: 
+            Data Frame with all variables available into the API XM 
+        """
+        request = {"MetricId": 'ListadoMetricas'}
+        connection = requests.post(f'http://servapibi.xm.com.co/Lists', json=request)
+        data_json = json.loads(connection.content)
+        df_variables = pd.json_normalize(data_json['Items'], 'ListEntities', 'Date', sep='_')
+        df_variables.drop(columns=['Id', 'Date'], inplace=True)
+        df_variables.columns = [x.replace('Values_', '') for x in df_variables.columns]
+        return df_variables 
     
-    def get_collections(self, coleccion):
-
-        return self.inventario_metricas[coleccion]
+    
+    def get_collections(self, coleccion=''):
+        """ request public server data from XM by the API
+        Args:
+            coleccion: optional parameter, one of the set of variables availables at self.get_collections()
+        Returns: 
+            DataFrame with the raw Data
+        """
+        try:
+            if coleccion == '':
+                return self.inventario_metricas
+            else:
+                return self.inventario_metricas.query("MetricId == '{}'".format(coleccion))
+        except:
+            print('No existe la métrica {}'.format(coleccion))
+            return pd.DataFrame()
+        
 
     def request_data(self, coleccion, metrica, start_date, end_date, filtros=None):
         """ request public server data from XM by the API
@@ -31,23 +68,25 @@ class ReadDB(object):
             start_date: start date consult data using YYYY-MM-DD format
             end_date: end date consult data using YYYY-MM-DD format
             filter: optional parameter, list of values to filter data
-        Returns: DataFrame with the raw Data
+        Returns: 
+            DataFrame with the raw Data
         """
         if type(filtros)==list:
             self.filtros = filtros
-        elif filtros ==None:
+        elif filtros == None:
             self.filtros=[]
         else:
             print('Los filtros deben ingresarse como una lista de valores')
             self.filtros = list
-        if coleccion not in self.inventario_metricas.keys():
-            print('No existe la colección {}'.format(coleccion))
+        if coleccion not in self.inventario_metricas.MetricId.values:
+            print('No existe la métrica {}'.format(coleccion))
             return pd.DataFrame()
-        if metrica > len(self.inventario_metricas[coleccion]):
-            print('No existe la metrica')
+        
+        if metrica not in self.inventario_metricas.Entity.values:
+            print('No existe la entidad {}'.format(metrica))
             return pd.DataFrame()
-
-        if self.inventario_metricas[coleccion][metrica][3] == 'Horaria':
+        
+        if self.inventario_metricas.query("MetricId == '{}' and Entity == '{}'".format(coleccion, metrica)).Type.values == 'HourlyEntities':
             end = end_date
             condition = True
             aux = True
@@ -62,7 +101,7 @@ class ReadDB(object):
                 self.request = {"MetricId": coleccion,
                                 "StartDate": "{}".format(str(start_date)),
                                 "EndDate": "{}".format(str(end)),
-                                'Entity': self.inventario_metricas[coleccion][metrica][2],
+                                'Entity': metrica,
                                 "Filter": self.filtros}
 
                 self.connection = requests.post(self.url, json=self.request)
@@ -81,7 +120,7 @@ class ReadDB(object):
                     aux = False
                 condition = ((end - start_date).days > 30 | (end - end_date).days != 0) | aux
 
-        elif self.inventario_metricas[coleccion][metrica][3] == 'Diaria':
+        elif self.inventario_metricas.query("MetricId == '{}' and Entity == '{}'".format(coleccion, metrica)).Type.values == 'DailyEntities':
             end = end_date
             condition = True
             aux = True
@@ -97,7 +136,7 @@ class ReadDB(object):
                 self.request = {"MetricId": coleccion,
                                 "StartDate": "{}".format(str(start_date)),
                                 "EndDate": "{}".format(str(end)),
-                                'Entity': self.inventario_metricas[coleccion][metrica][2],
+                                'Entity': metrica,
                                 "Filter": self.filtros}
                 self.connection = requests.post(self.url, json=self.request)
                 data_json = json.loads(self.connection.content)
@@ -111,8 +150,8 @@ class ReadDB(object):
                 if end == end_date:
                     aux = False
                 condition = ((end - start_date).days > 29 | (end - end_date).days != 0) | aux
-
-        elif self.inventario_metricas[coleccion][metrica][3] == 'Mensual':
+        
+        elif self.inventario_metricas.query("MetricId == '{}' and Entity == '{}'".format(coleccion, metrica)).Type.values == 'MonthlyEntities':
             
             end = end_date
             condition = True
@@ -129,7 +168,7 @@ class ReadDB(object):
                 self.request = {"MetricId": coleccion,
                                 "StartDate": "{}".format(str(start_date)),
                                 "EndDate": "{}".format(str(end)),
-                                'Entity': self.inventario_metricas[coleccion][metrica][2],
+                                'Entity': metrica,
                                 "Filter": self.filtros}
                 self.connection = requests.post(self.url, json=self.request)
                 data_json = json.loads(self.connection.content)
@@ -144,7 +183,7 @@ class ReadDB(object):
                     aux = False
                 condition = ((end - start_date).days > 731 | (end - end_date).days != 0) | aux
 
-        elif self.inventario_metricas[coleccion][metrica][3] == 'Anual':
+        elif self.inventario_metricas.query("MetricId == '{}' and Entity == '{}'".format(coleccion, metrica)).Type.values == 'AnnualEntities':
             
             end = end_date
             condition = True
@@ -161,7 +200,7 @@ class ReadDB(object):
                 self.request = {"MetricId": coleccion,
                                 "StartDate": "{}".format(str(start_date)),
                                 "EndDate": "{}".format(str(end)),
-                                'Entity': self.inventario_metricas[coleccion][metrica][2],
+                                'Entity': metrica,
                                 "Filter": self.filtros}
                 self.connection = requests.post(self.url, json=self.request)
                 data_json = json.loads(self.connection.content)
@@ -177,11 +216,11 @@ class ReadDB(object):
                 condition = ((end - start_date).days > 365 | (end - end_date).days != 0) | aux
 
 
-        elif self.inventario_metricas[coleccion][metrica][3] == 'Lista':
+        elif self.inventario_metricas.query("MetricId == '{}' and Entity == '{}'".format(coleccion, metrica)).Type.values == 'ListsEntities':
             period_base = 'lists'
             self.url = f'http://servapibi.xm.com.co/{period_base}'
             self.request = {'MetricId': coleccion,
-                            'Entity': self.inventario_metricas[coleccion][metrica][2]}
+                            'Entity': metrica}
   
             self.connection = requests.post(self.url, json=self.request)
             data_json = json.loads(self.connection.content)
@@ -197,12 +236,6 @@ class ReadDB(object):
 
 
 if __name__ == "__main__":
+    
     consult = ReadDB()
-    df1 = consult.request_data("Gene", 0, dt.date(2021, 1, 1), dt.date(2021, 1, 5))
-
-
-    
-    
-    
-    
-    
+    listado = consult.request_data("Gene", "Recurso", dt.date(2021, 1, 1), dt.date(2021, 1, 5), ['GTPE', 'SNCR'])
