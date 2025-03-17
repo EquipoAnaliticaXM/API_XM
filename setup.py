16bfc422aa073fcca56eacd5a3968d9653fcda1a
EDER DE JESUS PEREZ GARCIA <EPEREZ@XM.COM.CO>
2025-03-17 13:48:43 -0500

    smontoya - Adicion de funciones de variables y nuevo origen de archivo JSON

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pydataxm',         # How you named your package folder (MyLib)
   packages = ['pydataxm'],   # Chose the same as "name"
-  version = '0.3.12',      # Start with a small number and increase it with every change you make
+  version = '0.3.13',      # Start with a small number and increase it with every change you make
   license='MIT',        # Choose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Interface to play with the API XM',   # Give a short description about your library
   author = 'Equipo Analitica XM',                   # Type in your name
@@ -11,7 +11,7 @@ setup(
   download_url = 'https://raw.githubusercontent.com/EquipoAnaliticaXM/API_XM/master/pydataxm/pydataxm.py',    # I explain this later on
   keywords = ['API interpreter','Mercado Energía Mayorista', 'XM'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'requests', 'pandas','datetime', 'aiohttp', 'asyncio', 'nest_asyncio'],
+          'requests', 'pandas','datetime', 'aiohttp', 'asyncio', 'nest_asyncio', 'plotly'],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
